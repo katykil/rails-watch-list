12082333889b7624cbe4a39732bc86f96aa23f6a
KatyKilpatrick <katy.kilpatrick@hotmail.co.uk>
2021-11-12 12:58:40 +0000

    update

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,4 +3,7 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :comment, length: { minimum: 6 }
+  validates :movie, presence: true,
+                    uniquness: { scope: :list }
+  validates :list, presence: true
 end
--- db/migrate/20211111121422_add_overview_to_movies.rb
+++
@@ -1,4 +0,0 @@
-class AddOverviewToMovies < ActiveRecord::Migration[6.0]
-  def change
-  end
-end
